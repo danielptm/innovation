c35b10829e16aa7455d17ff301baceb5f91a9c75
Daniel <daniel.tuttle@nike.com>
2020-02-03 09:50:39 -0800

    Small changes

--- src/test/java/com/nike/innovation/innovation/InnovationApplicationTests.java
+++ src/test/java/com/nike/innovation/innovation/InnovationApplicationTests.java
@@ -1,7 +1,12 @@
 package com.nike.innovation.innovation;
 
+import jdk.nashorn.internal.ir.annotations.Ignore;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class InnovationApplicationTests {
--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sleep 25s
 cd terraform/local
 terraform init
 terraform apply -auto-approve
-#java -jar innovation.jar
+java -jar innovation.jar
